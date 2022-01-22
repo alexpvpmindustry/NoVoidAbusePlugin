e58a8f67a92c17587647e4052abd6a044734ad46
alex.mindustry <alex.mindustry@gmail.com>
2022-01-22 17:34:21 +0800

    final version

--- src/example/NoVoidPlugin.java
+++ src/example/NoVoidPlugin.java
@@ -1,69 +1,30 @@
 package example;
 
-import arc.*;
 import arc.util.*;
 import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
-import mindustry.net.Administration.*;
-import mindustry.world.blocks.storage.*;
 
 public class NoVoidPlugin extends Plugin{
-
     //called when game initializes
     @Override
     public void init(){
-        //listen for a block selection event
-        Events.on(BuildSelectEvent.class, event -> {
-            if(!event.breaking && event.builder != null && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer()){
-                //player is the unit controller
-                Player player = event.builder.getPlayer();
-
-                //send a message to everyone saying that this player has begun building a reactor
-                Call.sendMessage("[scarlet]ALERT![] " + player.name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
-            }
-        });
-
-        //add a chat filter that changes the contents of all messages
-        //in this case, all instances of "heck" are censored
-        Vars.netServer.admins.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
-
-        //add an action filter for preventing players from doing certain things
-        Vars.netServer.admins.addActionFilter(action -> {
-            //random example: prevent blast compound depositing
-            if(action.type == ActionType.depositItem && action.item == Items.blastCompound && action.tile.block() instanceof CoreBlock){
-                action.player.sendMessage("Example action filter: Prevents players from depositing blast compound into the core.");
-                return false;
-            }
-            return true;
-        });
+        new AntiVoid();
     }
 
-    //register commands that run on the server
-//    @Override
-//    public void registerServerCommands(CommandHandler handler){
-//        handler.register("reactors", "List all thorium reactors in the map.", args -> {
-//            for(int x = 0; x < Vars.world.width(); x++){
-//                for(int y = 0; y < Vars.world.height(); y++){
-//                    //loop through and log all found reactors
-//                    //make sure to only log reactor centers
-//                    if(Vars.world.tile(x, y).block() == Blocks.thoriumReactor && Vars.world.tile(x, y).isCenter()){
-//                        Log.info("Reactor at @, @", x, y);
-//                    }
-//                }
-//            }
-//        });
-//    }
-
-    //register commands that player can invoke in-game
-    @Override
-    public void registerClientCommands(CommandHandler handler){
-
-        //register a simple reply command
-        handler.<Player>register("testplugin", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
-            player.sendMessage("You said: [accent] " + args[0]);
-        });
+    class AntiVoid{
+        Timer.Task task;
+        public AntiVoid(){
+            int refreshInterval = 4; int buffer=5;
+            int h1 = -buffer;
+            int w1 = -buffer;
+            this.task = Timer.schedule(() -> {
+                int h2 = Vars.world.height()*8+buffer;
+                int w2 = Vars.world.width()*8+buffer;
+                Groups.unit.each(u -> ((u.x<w1) || (u.x>w2) || (u.y<h1) || (u.y>h2))
+                        , u -> { Call.sendMessage("Unit Out of Bounds. YEET.");
+                    u.kill();});
+            }, refreshInterval, refreshInterval);
+        }
     }
 }
